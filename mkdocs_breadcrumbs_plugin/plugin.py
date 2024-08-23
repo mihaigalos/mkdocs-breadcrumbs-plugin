2ca5abb7f57694720eb2b3170b19cff28d39461b
Mihai Galos <mihai.galos@rbinternational.com>
2024-08-23 20:57:39 +0200

    fix: Folders with no markdown - Recursive point to 1st markdown

--- mkdocs_breadcrumbs_plugin/plugin.py
+++ mkdocs_breadcrumbs_plugin/plugin.py
@@ -1,13 +1,9 @@
 import os
-import sys
-from timeit import default_timer as timer
-from datetime import datetime, timedelta
 import logging
 
 from mkdocs import utils as mkdocs_utils
 from mkdocs.config import config_options, Config
 from mkdocs.plugins import BasePlugin
-from mkdocs.structure.nav import get_navigation
 
 
 class BreadCrumbs(BasePlugin):
@@ -28,13 +24,20 @@ class BreadCrumbs(BasePlugin):
         self.logger.setLevel(numeric_level)
         self.logger.info(f'Log level set to {log_level}')
 
+    def _get_first_markdown_file(self, dir_path):
+        for root, _, files in os.walk(dir_path):
+            for file in sorted(files):
+                if file.endswith(".md"):
+                    return os.path.join(root, file)
+        return None
+
     def _get_first_document(self, config, ref_location):
         ref_path = os.path.join(config['docs_dir'], ref_location)
         if os.path.isdir(ref_path):
-            for file in os.listdir(ref_path):
-                if file.endswith(".md"):
-                    ref_location = f"{ref_location}/{file[:-3]}"
-                    break
+            first_md_file = self._get_first_markdown_file(ref_path)
+            if first_md_file:
+                ref_location = os.path.relpath(first_md_file, config['docs_dir']).replace('\\', '/')
+                ref_location = os.path.splitext(ref_location)[0]
         return ref_location
 
     def _get_base_url(self, config):
@@ -42,7 +45,6 @@ class BreadCrumbs(BasePlugin):
         base_url = ""
 
         if site_url:
-            print(site_url)
             temp = site_url.replace('http://', '').replace('https://', '')
             if "/" in temp:
                 base_url = temp.split('/', 1)[1]
@@ -60,10 +62,9 @@ class BreadCrumbs(BasePlugin):
         depth = 0
 
         while slashes > 0:
-            pos_slash = page.url.find("/", pos_start_substring+1)
+            pos_slash = page.url.find("/", pos_start_substring + 1)
             ref_name = page.url[pos_start_substring:pos_slash]
             ref_location = page.url[:pos_slash]
-
 
             ref_location = self._get_first_document(config, ref_location)
             self.logger.debug(f"page.url: {page.url} ref_name: {ref_name} ref_location: {ref_location} depth: {depth}, slashes: {slashes}")
@@ -72,12 +73,15 @@ class BreadCrumbs(BasePlugin):
                 breadcrumbs += self.config['delimiter']
             if depth > 0:
                 if self.base_url:
-                    breadcrumbs = breadcrumbs + f"[{ref_name}](/{self.base_url}/{ref_location}/)"
+                    breadcrumbs += f"[{ref_name}](/{self.base_url}/{ref_location}/)"
                 else:
-                    breadcrumbs = breadcrumbs + f"[{ref_name}](/{ref_location}/)"
+                    breadcrumbs += f"[{ref_name}](/{ref_location}/)"
 
             pos_start_substring = pos_slash + 1
             slashes -= 1
             depth += 1
+
+        home_breadcrumb = f"[Home](/{self.base_url}/)" if self.base_url else "[Home](/)"
+        breadcrumbs = home_breadcrumb + self.config['delimiter'] + breadcrumbs if breadcrumbs else home_breadcrumb
         return breadcrumbs + "\n" + markdown
 
