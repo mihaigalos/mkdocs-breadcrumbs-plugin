ea16b0f87968b7df8ab415847e79f00313221963
Mihai Galos <mihai.galos@rbinternational.com>
2025-02-26 16:33:40 +0100

    feat: Release 0.1.13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mkdocs-breadcrumbs-plugin",
-    version="0.1.12",
+    version="0.1.13",
     description="Location-based breadcrumbs plugin for mkdocs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
